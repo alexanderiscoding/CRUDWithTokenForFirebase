26927db81646e18a8b14d68bf5c60e99c5773481
alexanderfsm <94754185+alexanderfsm@users.noreply.github.com>
2022-06-10 20:17:44 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -10,4 +10,8 @@ if (!admin.apps.length) {
     console.log('Firebase admin initialization error', error.stack);
   }
 }
-export var db = admin.firestore();
+export const db = admin.firestore();
+// for token 32 caracters https://generate-secret.vercel.app/32
+// for token 64 caracters https://generate-secret.vercel.app/64
+// please change this token by an example above
+export const token =  "7f59ba5f69d5b4e588e0ab0d4f8e1634";
