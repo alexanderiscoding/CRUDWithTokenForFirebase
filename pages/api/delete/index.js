1628b13c4f1115e4745246a5ab2539feb7136818
alexanderfsm <94754185+alexanderfsm@users.noreply.github.com>
2022-06-10 21:53:20 -0300

    Create index.js

---
+++ pages/api/delete/index.js
@@ -0,0 +1,14 @@
+import { db, token } from '../../../firebaseConfig';
+
+export default async(req, res) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	if (!req.headers.authorization || req.headers.authorization.indexOf('Bearer ') === -1) {
+		return res.status(401).json({ message: 'Missing Authorization Header' });
+	}
+	const authorization =  req.headers.authorization.split(' ')[1];
+	if (authorization != token) {
+		return res.status(401).json({ message: 'Invalid Authentication Credentials' });
+	}
+	await db.collection(req.body.table.name).doc(req.body.column.id).delete();
+	res.status(200).send("Document successfully deleted!");
+}
