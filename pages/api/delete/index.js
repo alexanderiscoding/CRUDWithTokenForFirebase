21978e31257ddfe177a8d1de99040fd8e43c3673
alexanderfsm <94754185+alexanderfsm@users.noreply.github.com>
2022-06-10 22:55:22 -0300

    Update index.js

--- pages/api/delete/index.js
+++ pages/api/delete/index.js
@@ -1,4 +1,4 @@
-import { db, token } from '../../../firebaseConfig';
+import { db, token, fieldValue } from '../../../firebaseConfig';
 
 export default async(req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -9,6 +9,14 @@ export default async(req, res) => {
 	if (authorization != token) {
 		return res.status(401).json({ message: 'Invalid Authentication Credentials' });
 	}
-	await db.collection(req.body.table.name).doc(req.body.column.id).delete();
-	res.status(200).send("Document successfully deleted!");
+	if(req.body.column.delete === "true"){
+		const table = db.collection(req.body.table.name).doc(req.body.column.id);
+		await table.update({
+			[req.body.column.name]: fieldValue.delete()
+		});
+		res.status(200).send("Field successfully deleted!");
+	}else{
+		await db.collection(req.body.table.name).doc(req.body.column.id).delete();
+		res.status(200).send("Document successfully deleted!");
+	}
 }
