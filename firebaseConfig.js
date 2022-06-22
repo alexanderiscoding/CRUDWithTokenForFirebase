edc175dae711cc6135bdee1834969051e77ee879
alexanderfsm <94754185+alexanderfsm@users.noreply.github.com>
2022-06-21 21:08:10 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -16,3 +16,5 @@ export const fieldValue = admin.firestore.FieldValue;
 // for token 64 caracters https://generate-secret.vercel.app/64
 // please change this token by an example above
 export const token =  "7f59ba5f69d5b4e588e0ab0d4f8e1634";
+// please change this token by an example above
+export const encryptkey =  "c8f761d962dca2a5286dc846f04ddac643f5395d27d09b451f42410f3bc70571";
